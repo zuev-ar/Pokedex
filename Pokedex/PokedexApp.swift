1efa3a6368d3c42622bc51d85de4a0cb39b9cbef
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-11-23 21:49:54 +0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Arkasha Zuev on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
